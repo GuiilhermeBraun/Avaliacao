ffd0788212dc4f7fedaac6787230851a49fe7cf1
Guilherme Braun <guilherme.taschetto@duque.g12.br>
2024-10-07 21:28:46 -0300

    1 commit

---
+++ atvNovo/src/App.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+import SearchForm from "./componentes/SearchForm";
+import type { Repository } from "./types/github";
+import { searchRepositories } from "./services/github";
+import RepositoryList from "./componentes/RepositoryList";
+
+const App: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null> (null);
+
+  const handleSearch = async (query: string) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const result = await searchRepositories(query);
+      setRepositories(result.items);
+    } catch (err) {
+      const errorMessage = err instanceof Error ? err.message : 'Um erro desconhecido ocorreu';
+      setError(`Erro ao buscar repositórios: ${errorMessage}. Por favor, tente novamente.`);
+      setRepositories([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Pesquisa de Repositórios GitHub</h1>
+      <SearchForm onSearch={handleSearch} />
+      {loading && <p>Carregando...</p>}
+      {error && <p>{error}</p>}
+      {!loading && !error && <RepositoryList repositories={repositories} />}
+    </div>
+  );
+};
+
+export default App;
